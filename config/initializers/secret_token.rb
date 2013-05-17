96fab099bdb3f1091a59dad776c4ecfabf77274f
Pieter Coerman <p.coerman@nedforce.nl>
2013-05-17 16:41:34 +0200

    All your base are belong to us

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '67fc5542fb22ea67d90ed72e60767525538b2c6e0ab1134399d5141729fa9e73912fdcadd74f93576e9d671c21c87ae4dd0d0c418510aadab609e57071d1ba3b'
